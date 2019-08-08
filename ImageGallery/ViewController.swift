9e336922dbb84e0f81777019156717e4574f9cdd
Jack Sanford <jsanford9292@gmail.com>
2019-08-07 20:49:15 -0700

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by john sanford on 8/7/19.
+//  Copyright © 2019 Jack Sanford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
