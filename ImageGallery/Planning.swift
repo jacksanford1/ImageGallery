7ad30acf0de92fcc34bcaec25d710ebb6057e853
Jack Sanford <jsanford9292@gmail.com>
2019-08-07 22:49:27 -0700

    Created Table View Controller

---
+++ ImageGallery/Planning.swift
@@ -0,0 +1,45 @@
+/*
+ 
+ Planning ImageGallery:
+ 
+ EmojiArt app demo starts like 10 mins in to Lecture 11
+ 
+ Table view demo starts 15 mins from the end of Lecture 11
+ 
+ Collection view demo is in lecture 12
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
